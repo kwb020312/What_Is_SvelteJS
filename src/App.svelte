9e2aa8b7b4a18c2e205cb1d7d73197dcd1afb648
kimwoobin <kwb020312@naver.com>
2020-12-20 14:24:20 +0900

    on:click

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
-	const str = '<b>글씨를 굵게 줄거야</b>'
+	let count = 0;
+	const onClick = () => {
+		count++
+	}
 </script>
 
 <main>
-	<span>{@html str}</span>
+	<input type="button" on:click={onClick} value="카운트 업!" />
+	<p>{count} 회 누르셨습니다.</p>
 </main>
